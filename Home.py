f0c06ef5ea3cde45a287fed66518a798a12a00f3
FAKESFLAKES <133153614+FAKESFLAKES@users.noreply.github.com>
2023-07-27 17:08:09 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,22 +5,15 @@ def clear_screen():
 
 def display_header():
     header_text = "\033[91mEyes Of Security Group\033[0m"
-    disclaimer_text = "Script para fins educativos e testes, não me responsabilizo por nada"
+    disclaimer_text = "\033[92mScript para fins educativos e testes, não me responsabilizo por nada\033[0m"
     print(header_text.center(50))
     print(disclaimer_text.center(50))
-    print("\nOpções:")
-    print("1. Rodar o script ferramenta1.py")
-    print("2. Rodar o script ferramenta2.py")
-    print("3. Rodar o script ferramenta3.py")
-    print("4. Rodar o script ferramenta4.py")
-    print("5. Rodar o script ferramenta5.py")
-    print("0. Sair")
 
 def run_script(script_name):
     clear_screen()
     display_header()
 
-    print(f"\nExecutando o script {script_name}...\n")
+    print(f"\n\033[91mExecutando o script {script_name}...\033[0m\n")
     # Aqui você pode adicionar a lógica para executar o script correspondente
     # por exemplo: os.system(f"python {script_name}")
 
@@ -30,6 +23,14 @@ def main():
     while True:
         clear_screen()
         display_header()
+
+        print("\nOpções:")
+        print("1. Rodar o script ferramenta1.py")
+        print("2. Rodar o script ferramenta2.py")
+        print("3. Rodar o script ferramenta3.py")
+        print("4. Rodar o script ferramenta4.py")
+        print("5. Rodar o script ferramenta5.py")
+        print("0. Sair")
 
         try:
             option = int(input("\nEscolha uma opção: "))
